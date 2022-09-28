4b90536f830112d4e01bec87ac682500c5a38292
brucekid96 <kazebrice98@gmail.com>
2022-09-28 13:42:01 +0200

    finish to design cards in home page that lead to other pages

--- app/src/main/java/com/hackbettingowner/MainActivity.java
+++ app/src/main/java/com/hackbettingowner/MainActivity.java
@@ -13,6 +13,7 @@ import com.google.android.material.navigation.NavigationView;
 
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
 import androidx.core.view.GravityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -24,7 +25,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.hackbettingowner.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
-  private TextView text;
+  private CardView mAllTips;
+  private CardView mSafeTips;
+  private CardView mSingleTips;
+  private CardView mComboTips;
+  private CardView mFootballTips;
+  private CardView mBasketballTips;
 
 
 
@@ -32,12 +38,49 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    text = findViewById(R.id.home_text);
+
 
     Toolbar toolbar = findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
 
     DrawerLayout drawer =  findViewById(R.id.drawer_layout);
+    mAllTips = findViewById(R.id.alltips_card);
+    mSafeTips = findViewById(R.id.safetips_card);
+    mSingleTips = findViewById(R.id.singletips_card);
+    mComboTips = findViewById(R.id.combotips_card);
+    mFootballTips = findViewById(R.id.footballtips_card);
+    mBasketballTips = findViewById(R.id.basketballtips_card);
+
+    mAllTips.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+        startActivity(new Intent(MainActivity.this, AllTips.class));
+      }
+    });
+    mSafeTips.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+        startActivity(new Intent(MainActivity.this, SafeTips.class));
+      }
+    });
+    mSingleTips.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+        startActivity(new Intent(MainActivity.this, SingleTips.class));
+      }
+    });
+    mComboTips.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+        startActivity(new Intent(MainActivity.this, ComboTips.class));
+      }
+    });
+    mFootballTips.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+        startActivity(new Intent(MainActivity.this, FootballTips.class));
+      }
+    });
+    mBasketballTips.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+        startActivity(new Intent(MainActivity.this, BasketballTips.class));
+      }
+    });
 
     ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
         this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
